6df5b0f0320c3e346553f5d3c6971dd0f57b4766
dannyLiu <h152074716@gmail.com>
2020-11-07 13:22:02 +0800

    匯入AuthenticationService範例

---
+++ UnitTest91/Context.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace UnitTest91
+{
+    public static class Context
+    {
+        public static Dictionary<string, string> profiles;
+
+        static Context()
+        {
+            profiles = new Dictionary<string, string>();
+            profiles.Add("joey", "91");
+            profiles.Add("mei", "99");
+        }
+
+        public static string GetPassword(string key)
+        {
+            return profiles[key];
+        }
+    }
+}
---
+++ UnitTest91/ProfileDao.cs
@@ -0,0 +1,10 @@
+﻿namespace UnitTest91
+{
+    public class ProfileDao
+    {
+        public string GetPassword(string account)
+        {
+            return Context.GetPassword(account);
+        }
+    }
+}
