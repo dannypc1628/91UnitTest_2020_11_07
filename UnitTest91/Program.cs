ce79ef4efc397802dcd01484e5b4577c956e2c25
dannyLiu <h152074716@gmail.com>
2020-11-07 10:58:04 +0800

    加入專案檔案。

---
+++ UnitTest91/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace UnitTest91
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Holiiday
+    {
+        
+
+        public string SayHello()
+        {
+            var _day = GetToday();
+            if (_day.Month == 12 && _day.Day == 25)
+            {
+                return "Merry Xmas";
+            }
+
+            return "Today is not Xmas";
+        }
+
+        protected virtual DateTime GetToday()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ UnitTest91Tests/HoliidayTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest91;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest91.Tests
+{
+    [TestClass()]
+    public class HoliidayTests
+    {
+        [TestMethod()]
+        public void SayHello_Xms_Yes_Test()
+        {
+            var expect = "Merry Xmas";
+            var holiday = new HoliidayTest(){ _toDay = DateTime.Now};
+            var actual = holiday.SayHello();
+
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void SayHello_Xms_No_Test()
+        {
+            var expect = "Today is not Xmas";
+            var holiday = new HoliidayTest() { _toDay = DateTime.Now };
+            var actual = holiday.SayHello();
+
+            Assert.AreEqual(expect, actual);
+        }
+    }
+
+    class HoliidayTest : Holiiday
+    {
+        public DateTime _toDay;
+        protected override DateTime GetToday()
+        {
+            return _toDay;
+        }
+    }
+}
