1889a692056bcd4d813f7f2da378d047186d8f1b
dannyLiu <h152074716@gmail.com>
2020-11-07 14:06:42 +0800

    建立AuthenticationService單元測試並匯入FakeProfile與FakeToken

---
+++ UnitTest91Tests/AuthenticationServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest91;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest91.Tests
+{
+    [TestClass()]
+    public class AuthenticationServiceTests
+    {
+        [TestMethod()]
+        public void AuthenticationServiceTest()
+        {
+            var target = new AuthenticationService(new FakeProfile(), new FakeToken());
+            var actual = target.IsValid("joey", "91000000");
+            Assert.IsTrue(actual);
+        }
+
+        internal class FakeProfile : IProfile
+        {
+            public string GetPassword(string account)
+            {
+                if (account == "joey")
+                {
+                    return "91";
+                }
+
+                return "";
+            }
+        }
+
+        internal class FakeToken : IToken
+        {
+            public string GetRandom(string account)
+            {
+                return "000000";
+            }
+        }
+    }
+}
