db0df54db7adf9f0a5bdeb568ad23d6c39585e6d
dannyLiu <h152074716@gmail.com>
2020-11-07 14:22:06 +0800

    使用NSubstitute建立假物件

--- UnitTest91Tests/AuthenticationServiceTests.cs
+++ UnitTest91Tests/AuthenticationServiceTests.cs
@@ -3,6 +3,7 @@ using UnitTest91;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using NSubstitute;
 
 namespace UnitTest91.Tests
 {
@@ -12,30 +13,35 @@ namespace UnitTest91.Tests
         [TestMethod()]
         public void AuthenticationServiceTest()
         {
-            var target = new AuthenticationService(new FakeProfile(), new FakeToken());
+            var stubProfile = Substitute.For<IProfile>();
+            stubProfile.GetPassword("joey").Returns("91");
+            var stubToken = Substitute.For<IToken>();
+            stubToken.GetRandom("").ReturnsForAnyArgs("000000");
+
+            var target = new AuthenticationService(stubProfile, stubToken);
             var actual = target.IsValid("joey", "91000000");
             Assert.IsTrue(actual);
         }
 
-        internal class FakeProfile : IProfile
-        {
-            public string GetPassword(string account)
-            {
-                if (account == "joey")
-                {
-                    return "91";
-                }
+        //internal class FakeProfile : IProfile
+        //{
+        //    public string GetPassword(string account)
+        //    {
+        //        if (account == "joey")
+        //        {
+        //            return "91";
+        //        }
 
-                return "";
-            }
-        }
+        //        return "";
+        //    }
+        //}
 
-        internal class FakeToken : IToken
-        {
-            public string GetRandom(string account)
-            {
-                return "000000";
-            }
-        }
+        //internal class FakeToken : IToken
+        //{
+        //    public string GetRandom(string account)
+        //    {
+        //        return "000000";
+        //    }
+        //}
     }
 }
